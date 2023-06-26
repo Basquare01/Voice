b7998eb7c9be82eab975b91a1c9e9dbb0e44dbc4
Basquare <bilalhassansule@gmail.com>
2023-06-26 22:35:12 +0100

    Voice-Converter-To-Text

---
+++ voice.js
@@ -0,0 +1,18 @@
+let speech = new SpeechSynthesisUtterance();
+
+let voices = [];
+let chooseVoice = document.querySelector('select');
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+    voices.forEach((voice, a) => (chooseVoice.options[a] = new Option(voice.name, a)))
+}
+
+chooseVoice.addEventListener('change', () => {
+    speech.voice = voices[chooseVoice.value]
+})
+
+document.querySelector('button').addEventListener('click', () => {
+    speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.speak(speech)
+})
